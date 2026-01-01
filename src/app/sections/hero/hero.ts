12f44095edf1141b0ea414ddbe95f4bacafca70b
adamkosovic <adam.kosovic@icloud.com>
2026-01-01 16:46:53 +0100

    Animation

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TextSwitchComponent } from '../../shared/text-switch/text-switch';
 import { BrandIcons } from '../../shared/brand-icons/brand-icons';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 
 @Component({
@@ -8,6 +9,14 @@ import { BrandIcons } from '../../shared/brand-icons/brand-icons';
   imports: [TextSwitchComponent, BrandIcons],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
+  animations: [
+    trigger('fadeSlideUp', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-60px)' }),
+        animate('700ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ])
+  ],
 })
 export class Hero {
 
