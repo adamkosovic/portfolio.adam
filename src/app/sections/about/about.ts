edd61311cb29bdc8632ddec4bdc73d4ba917b92f
adamkosovic <adam.kosovic@icloud.com>
2026-01-02 21:21:45 +0100

    scroll tech stack

--- src/app/sections/about/about.ts
+++ src/app/sections/about/about.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Tech } from '../../shared/tech/tech';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Tech],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
