8fa93dd03ab70fda42cc8864d60ce22e3af88ad4
adamkosovic <adam.kosovic@icloud.com>
2026-01-18 21:31:02 +0100

    Begining of section btn

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { TextSwitchComponent } from '../../shared/text-switch/text-switch';
 import { BrandIcons } from '../../shared/brand-icons/brand-icons';
-import { trigger, transition, style, animate } from '@angular/animations';
 
 
 @Component({
@@ -10,39 +9,5 @@ import { trigger, transition, style, animate } from '@angular/animations';
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {
-  private raf = 0;
-
-  tilt(e: PointerEvent, el: HTMLElement) {
-    const rect = el.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-  
-    const px = x / rect.width - 0.5;
-    const py = y / rect.height - 0.5;
-  
-    const maxTilt = 7;
-  
-    const ry = px * maxTilt;
-    const rx = -py * maxTilt;
-  
-    el.style.setProperty('--mx', `${x}px`);
-    el.style.setProperty('--my', `${y}px`);
-  
-    cancelAnimationFrame(this.raf);
-    this.raf = requestAnimationFrame(() => {
-      el.style.setProperty('--t', '0ms');
-      el.style.setProperty('--rx', `${rx}deg`);
-      el.style.setProperty('--ry', `${ry}deg`);
-    });
-  }
-  
-  resetTilt(el: HTMLElement) {
-    cancelAnimationFrame(this.raf);
-    el.style.setProperty('--t', '240ms');
-    el.style.setProperty('--rx', `0deg`);
-    el.style.setProperty('--ry', `0deg`);
-  }
-  
-    
+export class Hero{
 }
---
+++ src/app/service/scroll-sections/scroll-sections.ts
@@ -0,0 +1,6 @@
+import { Injectable, ElementRef, Inject } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class ScrollSections {
+
+}
