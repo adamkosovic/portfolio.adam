e1fee78e4f97e8f53cfb160e4b03e6d591414ca5
adamkosovic <adam.kosovic@icloud.com>
2026-01-27 17:14:29 +0100

    After send contact form

--- src/app/sections/contact/contact.ts
+++ src/app/sections/contact/contact.ts
@@ -9,5 +9,35 @@ import { ScrollSectionDirective } from '../../directive/scroll-section';
   styleUrl: './contact.css',
 })
 export class Contact {
+  success = false;
+  error = false;
+  loading = false;
 
+  async submit(event: Event) {
+    event.preventDefault();
+  
+    const form = event.target as HTMLFormElement;
+    if (!form) return;
+  
+    this.success = false;
+    this.error = false;
+    this.loading = true;
+  
+    try {
+      const res = await fetch('https://formspree.io/f/meegnakz', {
+        method: 'POST',
+        body: new FormData(form),
+        headers: { Accept: 'application/json' },
+      });
+  
+      if (!res.ok) throw new Error('Failed');
+  
+      this.success = true;
+      form.reset();
+    } catch {
+      this.error = true;
+    } finally {
+      this.loading = false;
+    }
+  }
 }
