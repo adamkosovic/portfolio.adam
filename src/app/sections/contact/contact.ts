c267b869458f38786380e61b8189e6214577cc44
adamkosovic <adam.kosovic@icloud.com>
2026-01-28 10:50:20 +0100

    Contact form

--- src/app/sections/contact/contact.ts
+++ src/app/sections/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone } from '@angular/core';
+import { Component } from '@angular/core';
 import { ScrollSectionDirective } from '../../directive/scroll-section';
 
 @Component({
@@ -9,45 +9,4 @@ import { ScrollSectionDirective } from '../../directive/scroll-section';
   styleUrl: './contact.css',
 })
 export class Contact {
-  loading = false;
-  success = false;
-  error = false;
-
-  constructor(private zone: NgZone) {}
-
-  async submit(event: Event) {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement | null;
-    if (!form) return;
-
-    // Sätt state INNE i zonen
-    this.zone.run(() => {
-      this.loading = true;
-      this.success = false;
-      this.error = false;
-    });
-
-    try {
-      const res = await fetch('https://formspree.io/f/meegnakz', {
-        method: 'POST',
-        body: new FormData(form),
-        headers: { Accept: 'application/json' },
-      });
-
-      if (!res.ok) throw new Error('Failed');
-
-      this.zone.run(() => {
-        this.success = true;
-        form.reset();
-      });
-    } catch (e) {
-      this.zone.run(() => {
-        this.error = true;
-      });
-    } finally {
-      this.zone.run(() => {
-        this.loading = false; // <-- nu släpper "Sending..."
-      });
-    }
-  }
 }
