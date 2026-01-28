af9fb19ec7a4a86927db8a9e9726eb182541a951
adamkosovic <adam.kosovic@icloud.com>
2026-01-28 20:24:49 +0100

    removed hover effect on phone

--- src/app/shared/project-cards/project-cards.ts
+++ src/app/shared/project-cards/project-cards.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Project} from '../../model/project.model';
+import { Project } from '../../model/project.model';
 
 @Component({
   selector: 'app-project-cards',
@@ -14,27 +14,35 @@ export class ProjectCardsComponent {
   @Input() reverse = false;
 
   tilt(e: MouseEvent, el: HTMLElement) {
+    if (!window.matchMedia('(hover: hover)').matches) {
+      return;
+    }
+
     const rect = el.getBoundingClientRect();
 
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-  
+
     const px = x / rect.width - 0.5;
     const py = y / rect.height - 0.5;
-  
-    const maxTilt = 12; 
-  
-    const rotateY = px * maxTilt;     
-    const rotateX = -py * maxTilt;   
-  
+
+    const maxTilt = 12;
+
+    const rotateY = px * maxTilt;
+    const rotateX = -py * maxTilt;
+
     el.style.setProperty('--rx', `${rotateX}deg`);
     el.style.setProperty('--ry', `${rotateY}deg`);
-  
+
     el.style.setProperty('--mx', `${x}px`);
     el.style.setProperty('--my', `${y}px`);
   }
-  
+
   resetTilt(el: HTMLElement) {
+    if (!window.matchMedia('(hover: hover)').matches) {
+      return;
+    }
+
     el.style.setProperty('--rx', `0deg`);
     el.style.setProperty('--ry', `0deg`);
   }
